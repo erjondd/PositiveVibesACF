1b1a8a8d5e4d3f8010b5cd9d5cdf8df1e14c27ce
Erjon <erjon.dinarama@gmail.com>
2025-10-01 20:27:33 +0200

    Links page finished

---
+++ template-links.php
@@ -0,0 +1,209 @@
+<?php
+
+get_header();
+
+/*
+Template Name: Links Template
+*/
+
+?>
+<section class="hero">
+    <div class="container">
+        <div class="title">Links</div>
+    </div>
+</section>
+<section class="links">
+    <div class="container">
+        <div class="all-links">
+            <div class="one-link">
+                <div class="image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/link1.png"
+                        alt="<?php bloginfo('name'); ?>">
+                </div>
+                <div class="title">
+                    Digital mental health resources
+                </div>
+                <div class="button">
+                    <a href="">
+                        CHECK WEBSITE
+                    </a>
+                </div>
+            </div>
+            <div class="one-link">
+                <div class="image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/link2.png"
+                        alt="<?php bloginfo('name'); ?>">
+                </div>
+                <div class="title">
+                    Has valuable information on Depression and Bipolar affective disorders
+
+                </div>
+                <div class="button">
+                    <a href="">
+                        CHECK WEBSITE
+                    </a>
+                </div>
+            </div>
+            <div class="one-link">
+                <div class="image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/link3.png"
+                        alt="<?php bloginfo('name'); ?>">
+                </div>
+                <div class="title">
+                    Useful information about Depression by the national body for Depression awareness and initiatives
+
+                </div>
+                <div class="button">
+                    <a href="">
+                        CHECK WEBSITE
+                    </a>
+                </div>
+            </div>
+            <div class="one-link">
+                <div class="image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/link4.png"
+                        alt="<?php bloginfo('name'); ?>">
+                </div>
+                <div class="title">
+                    Information regarding depression
+                </div>
+                <div class="button">
+                    <a href="">
+                        CHECK WEBSITE
+                    </a>
+                </div>
+            </div>
+            <div class="one-link">
+                <div class="image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/link5.png"
+                        alt="<?php bloginfo('name'); ?>">
+                </div>
+                <div class="title">
+                    Information on drug and alcohol addictions and treatments
+                </div>
+                <div class="button">
+                    <a href="">
+                        CHECK WEBSITE
+                    </a>
+                </div>
+            </div>
+            <div class="one-link">
+                <div class="image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/link6.png"
+                        alt="<?php bloginfo('name'); ?>">
+                </div>
+                <div class="title">
+                    The national charity for people affected by mental illness, and contains useful information
+                </div>
+                <div class="button">
+                    <a href="">
+                        CHECK WEBSITE
+                    </a>
+                </div>
+            </div>
+            <div class="one-link">
+                <div class="image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/link7.png"
+                        alt="<?php bloginfo('name'); ?>">
+                </div>
+                <div class="title">
+                    Royal Australian and New Zealand College of Psychiatrists website
+                </div>
+                <div class="button">
+                    <a href="">
+                        CHECK WEBSITE
+                    </a>
+                </div>
+            </div>
+            <div class="one-link">
+                <div class="image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/link8.png"
+                        alt="<?php bloginfo('name'); ?>">
+                </div>
+                <div class="title">
+                    Information about the schizophrenia and carer support groups
+                </div>
+                <div class="button">
+                    <a href="">
+                        CHECK WEBSITE
+                    </a>
+                </div>
+            </div>
+            <div class="one-link">
+                <div class="image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/link9.png"
+                        alt="<?php bloginfo('name'); ?>">
+                </div>
+                <div class="title">
+                    Information about dementia
+                </div>
+                <div class="button">
+                    <a href="">
+                        CHECK WEBSITE
+                    </a>
+                </div>
+            </div>
+            <div class="one-link">
+                <div class="image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/link10.png"
+                        alt="<?php bloginfo('name'); ?>">
+                </div>
+                <div class="title">
+                    Government established National Youth Mental Health Foundation, providing mental and health wellbeing support, information and services to young people
+
+
+
+                </div>
+                <div class="button">
+                    <a href="">
+                        CHECK WEBSITE
+                    </a>
+                </div>
+            </div>
+            <div class="one-link">
+                <div class="image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/link11.png"
+                        alt="<?php bloginfo('name'); ?>">
+                </div>
+                <div class="title">
+                    24hr crisis line · Suicide prevention · Self-help tool kits · Find services in your local area · Join our mailing list · Volunteer · Corporate  engagement
+                </div>
+                <div class="button">
+                    <a href="">
+                        CHECK WEBSITE
+                    </a>
+                </div>
+            </div>
+            <div class="one-link">
+                <div class="image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/link12.png"
+                        alt="<?php bloginfo('name'); ?>">
+                </div>
+                <div class="title">
+                    Online government service to help you find the right Australian digital mental health resources.
+                </div>
+                <div class="button">
+                    <a href="">
+                        CHECK WEBSITE
+                    </a>
+                </div>
+            </div>
+            <div class="one-link">
+                <div class="image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/link13.png"
+                        alt="<?php bloginfo('name'); ?>">
+                </div>
+                <div class="title">
+                    Drug and alcohol treatment programmes in Australia </div>
+                <div class="button">
+                    <a href="">
+                        CHECK WEBSITE
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php
+get_footer();
+?>
