6718fcdf9eb48a2bca79d544745dfe4b993dd801
Erjon <erjon.dinarama@gmail.com>
2025-10-01 16:10:44 +0200

    Adding Hero Section

--- template-home.php
+++ template-home.php
@@ -12,10 +12,49 @@ Template Name: Homepage Template
 
 <section class="hero">
     <div class="container">
-
+        <div class="subtitle">
+            Sparking conversations to improve lives
+        </div>
+        <div class="title">
+            Promoting positive mental health
+        </div>
+        <div class="button">
+            <a href="">
+                GET INVOLVED
+            </a>
+        </div>
     </div>
 </section>
-
+<section class="logos">
+    <div class="container">
+        <div class="all-logos">
+            <div class="one-logo">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/logo1.png"
+                    alt="<?php bloginfo('name'); ?>">
+            </div>
+            <div class="one-logo">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/logo2.png"
+                    alt="<?php bloginfo('name'); ?>">
+            </div>
+            <div class="one-logo">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/logo3.png"
+                    alt="<?php bloginfo('name'); ?>">
+            </div>
+            <div class="one-logo">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/logo4.png"
+                    alt="<?php bloginfo('name'); ?>">
+            </div>
+            <div class="one-logo">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/logo5.png"
+                    alt="<?php bloginfo('name'); ?>">
+            </div>
+            <div class="one-logo">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/logo6.png"
+                    alt="<?php bloginfo('name'); ?>">
+            </div>
+        </div>
+    </div>
+</section>
 <?php
 get_footer();
 ?>
