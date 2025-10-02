eccfc832f48f14f9180bb98c187deb482b617686
Erjon <erjon.dinarama@gmail.com>
2025-10-02 14:46:30 +0200

    Creating new pages

---
+++ template-get-involved.php
@@ -0,0 +1,96 @@
+<?php
+
+get_header();
+
+/*
+Template Name: Get Involved Template
+*/
+
+?>
+<section class="hero">
+    <div class="container">
+        <div class="title">Become A Volunteer</div>
+    </div>
+</section>
+<section class="get-involved">
+    <div class="container">
+        <div class="title">
+            Volunteering Opportunities
+        </div>
+        <div class="subtitle">
+            Join us
+        </div>
+        <div class="get-involved-text">
+            Would you like to get involved with the Positive Vibes Foundation?<br /> Here’s how you can join us.
+        </div>
+        <div class="all-involved">
+            <div class="one-involved">
+                <div class="one-image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/inv1.png"
+                        alt="<?php bloginfo('name'); ?>">
+                </div>
+                <div class="one-title">
+                    We are always on the lookout for more volunteers to join the Paw Visits program, visiting aged care homes with our furry friends.
+                </div>
+                <div class="one-link">
+                    <a href="">
+                        Become Volunteer
+                        <span><img src="<?php echo get_template_directory_uri(); ?>/images/vector.png"
+                                alt="<?php bloginfo('name'); ?>"></span>
+                    </a>
+                </div>
+            </div>
+            <div class="one-involved">
+                <div class="one-image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/inv1.png"
+                        alt="<?php bloginfo('name'); ?>">
+                </div>
+                <div class="one-title">
+                    We are always on the lookout for more volunteers to join the Paw Visits program, visiting aged care homes with our furry friends.
+                </div>
+                <div class="one-link">
+                    <a href="">
+                        Become Volunteer
+                        <span><img src="<?php echo get_template_directory_uri(); ?>/images/vector.png"
+                                alt="<?php bloginfo('name'); ?>"></span>
+                    </a>
+                </div>
+            </div>
+            <div class="one-involved">
+                <div class="one-image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/inv1.png"
+                        alt="<?php bloginfo('name'); ?>">
+                </div>
+                <div class="one-title">
+                    We are always on the lookout for more volunteers to join the Paw Visits program, visiting aged care homes with our furry friends.
+                </div>
+                <div class="one-link">
+                    <a href="">
+                        Become Volunteer
+                        <span><img src="<?php echo get_template_directory_uri(); ?>/images/vector.png"
+                                alt="<?php bloginfo('name'); ?>"></span>
+                    </a>
+                </div>
+            </div>
+            <div class="one-involved">
+                <div class="one-image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/inv1.png"
+                        alt="<?php bloginfo('name'); ?>">
+                </div>
+                <div class="one-title">
+                    We are always on the lookout for more volunteers to join the Paw Visits program, visiting aged care homes with our furry friends.
+                </div>
+                <div class="one-link">
+                    <a href="">
+                        Become Volunteer
+                        <span><img src="<?php echo get_template_directory_uri(); ?>/images/vector.png"
+                                alt="<?php bloginfo('name'); ?>"></span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php
+get_footer();
+?>
---
+++ template-teams.php
@@ -0,0 +1,168 @@
+<?php
+
+get_header();
+
+/*
+Template Name: Teams Template
+*/
+
+?>
+<section class="hero">
+    <div class="container">
+        <div class="title">Teams</div>
+    </div>
+</section>
+<section class="team">
+    <div class="container">
+        <div class="title">The Positive Vibes Foundation’s team</div>
+        <div class="all-team">
+            <div class="a-team">
+                <div class="image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/team1.png"
+                        alt="<?php bloginfo('name'); ?>">
+                </div>
+                <div class="a-title">
+                    Albert Seah FCA, CTA, LL.M, LL.B, B.Ec (Acctg)
+                </div>
+                <div class="a-position">
+                    Director
+                </div>
+            </div>
+            <div class="a-team">
+                <div class="image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/team2.png"
+                        alt="<?php bloginfo('name'); ?>">
+                </div>
+                <div class="a-title">
+                    Alexia Yazdani
+                </div>
+                <div class="a-position">
+                    Director
+                </div>
+            </div>
+            <div class="a-team">
+                <div class="image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/team1.png"
+                        alt="<?php bloginfo('name'); ?>">
+                </div>
+                <div class="a-title">
+                    Albert Seah FCA, CTA, LL.M, LL.B, B.Ec (Acctg)
+                </div>
+                <div class="a-position">
+                    Director
+                </div>
+            </div>
+            <div class="a-team">
+                <div class="image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/team1.png"
+                        alt="<?php bloginfo('name'); ?>">
+                </div>
+                <div class="a-title">
+                    Albert Seah FCA, CTA, LL.M, LL.B, B.Ec (Acctg)
+                </div>
+                <div class="a-position">
+                    Director
+                </div>
+            </div>
+            <div class="a-team">
+                <div class="image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/team1.png"
+                        alt="<?php bloginfo('name'); ?>">
+                </div>
+                <div class="a-title">
+                    Albert Seah FCA, CTA, LL.M, LL.B, B.Ec (Acctg)
+                </div>
+                <div class="a-position">
+                    Director
+                </div>
+            </div>
+            <div class="a-team">
+                <div class="image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/team1.png"
+                        alt="<?php bloginfo('name'); ?>">
+                </div>
+                <div class="a-title">
+                    Albert Seah FCA, CTA, LL.M, LL.B, B.Ec (Acctg)
+                </div>
+                <div class="a-position">
+                    Director
+                </div>
+            </div>
+            <div class="a-team">
+                <div class="image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/team1.png"
+                        alt="<?php bloginfo('name'); ?>">
+                </div>
+                <div class="a-title">
+                    Albert Seah FCA, CTA, LL.M, LL.B, B.Ec (Acctg)
+                </div>
+                <div class="a-position">
+                    Director
+                </div>
+            </div>
+            <div class="a-team">
+                <div class="image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/team1.png"
+                        alt="<?php bloginfo('name'); ?>">
+                </div>
+                <div class="a-title">
+                    Albert Seah FCA, CTA, LL.M, LL.B, B.Ec (Acctg)
+                </div>
+                <div class="a-position">
+                    Director
+                </div>
+            </div>
+            <div class="a-team">
+                <div class="image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/team1.png"
+                        alt="<?php bloginfo('name'); ?>">
+                </div>
+                <div class="a-title">
+                    Albert Seah FCA, CTA, LL.M, LL.B, B.Ec (Acctg)
+                </div>
+                <div class="a-position">
+                    Director
+                </div>
+            </div>
+            <div class="a-team">
+                <div class="image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/team1.png"
+                        alt="<?php bloginfo('name'); ?>">
+                </div>
+                <div class="a-title">
+                    Albert Seah FCA, CTA, LL.M, LL.B, B.Ec (Acctg)
+                </div>
+                <div class="a-position">
+                    Director
+                </div>
+            </div>
+            <div class="a-team">
+                <div class="image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/team1.png"
+                        alt="<?php bloginfo('name'); ?>">
+                </div>
+                <div class="a-title">
+                    Albert Seah FCA, CTA, LL.M, LL.B, B.Ec (Acctg)
+                </div>
+                <div class="a-position">
+                    Director
+                </div>
+            </div>
+            <div class="a-team">
+                <div class="image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/team1.png"
+                        alt="<?php bloginfo('name'); ?>">
+                </div>
+                <div class="a-title">
+                    Albert Seah FCA, CTA, LL.M, LL.B, B.Ec (Acctg)
+                </div>
+                <div class="a-position">
+                    Director
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php
+get_footer();
+?>
